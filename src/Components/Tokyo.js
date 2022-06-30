705720e6da1eca0ad0de15fdb3406df0fb2f13ef
zenstacker <zenstacker@gmail.com>
2022-06-30 17:34:26 +0900

    fifth commit

--- src/Components/Tokyo.js
+++ src/Components/Tokyo.js
@@ -1,5 +1,29 @@
 import { useQuery } from "react-query";
+import styled from "styled-components";
 import { fetchTokyo } from "../api";
+
+const WeatherWrapper = styled.div`
+  width: 580px;
+  margin: 0 auto;
+  display: grid;
+  justify-content: center;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 24px;
+`;
+
+const HumadityDiv = styled.div`
+  color: black;
+  height: 70px;
+  margin-top: 40px;
+  border: none;
+  border-radius: 10px;
+  background-color: whitesmoke;
+  text-align: center;
+  padding: 20px 10px;
+  display: grid;
+  grid-template-rows: auto;
+  gap: 6px;
+`;
 
 function Tokyo() {
   const { isLoading, data } = useQuery(
@@ -12,7 +36,16 @@ function Tokyo() {
 
   console.log(data);
 
-  return <h1>Tokyo</h1>;
+  return (
+    <WeatherWrapper>
+      <HumadityDiv>
+        <div>Humanity</div>
+        <div>432</div>
+      </HumadityDiv>
+      <HumadityDiv></HumadityDiv>
+      <HumadityDiv></HumadityDiv>
+    </WeatherWrapper>
+  );
 }
 
 export default Tokyo;
